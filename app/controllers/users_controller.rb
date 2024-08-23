655060a5e6baf579db9cea2a930639b3b2809283
Adrian Perez <adrian1804perez@gmail.com>
2024-08-23 15:39:55 -0700

    Add a user search field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @sent_requests = @user.requests
     @follow_requests = @user.requesters
+    @searched_users = User.where(first_name: params[:user_first]).or(User.where(last_name: params[:user_last]))
   end
 end
