7369d9b006ebb6e9cd974dc44bfa0a258f3a653a
Adrian Perez <adrian1804perez@gmail.com>
2024-08-25 15:20:17 -0700

    Displaying followers and following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @sent_requests = Request.where(requester_id: @user.id).all.includes(:requested)
     @follow_requests = Request.where(requested_id: @user.id).all.includes(:requester)
+    @followers = Following.where(followee_id: @user.id).all.includes(:follower)
+    @following = Following.where(follower_id: @user.id).all.includes(:followee)
     @searched_users = search_users
   end
 
