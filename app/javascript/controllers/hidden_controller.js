83f9e32d66a5d38c56d54b2d6dca2893f4c7c4f9
Adrian Perez <adrian1804perez@gmail.com>
2024-08-27 09:15:58 -0700

    Add hidden stimulus controller

---
+++ app/javascript/controllers/hidden_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="hidden"
+export default class extends Controller {
+  // This hidden controller chooses the element to hide based on class name
+  static targets = ["input", "field"]
+  connect() {
+  }
+
+  toggle(event) {
+    let keepOnId = event.target.value
+    this.fieldTargets.forEach((field) => {
+      let fieldId = field.id
+      if (fieldId === keepOnId) {
+        field.classList.remove('hidden')
+      } else {
+        field.classList.add('hidden')
+      }
+    })
+  }
+}
