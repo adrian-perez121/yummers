5077b580530fb63bd040f01ba5d863886192e3a6
Adrian Perez <adrian1804perez@gmail.com>
2024-08-27 17:50:17 -0700

    Change default scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_one_attached :image
 
+  default_scope { order(created_at: :desc)}
+
   validates :description, :author_id, :recipe, presence: true, on: [:create, :update]
   validates :description, :recipe, length: { in: 50..500 }
 end
