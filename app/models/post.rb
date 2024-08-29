a106c7dd2ada9ea79ec2178b207e213325cd02cb
Adrian Perez <adrian1804perez@gmail.com>
2024-08-29 15:31:33 -0700

    Add class variables to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,20 @@
 class Post < ApplicationRecord
+  @@min_length = 50
+  @@max_length = 500
+
   belongs_to :author, class_name: 'User'
   has_one_attached :image
 
   default_scope { order(created_at: :desc)}
 
   validates :description, :author_id, :recipe, presence: true, on: [:create, :update]
-  validates :description, :recipe, length: { in: 50..500 }
+  validates :description, :recipe, length: { in: @@min_length..@@max_length }
+
+  def self.min_length
+    @@min_length
+  end
+
+  def self.max_length
+    @@max_length
+  end
 end
