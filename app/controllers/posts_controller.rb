dbfd2121fddf84407ba4f9453bfabd1078a86f5d
Adrian Perez <adrian1804perez@gmail.com>
2024-08-31 11:51:26 -0700

    Add destroy method for posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+    @post.destroy!
+
+    redirect_to @user
+  end
   private
 
   def post_params
