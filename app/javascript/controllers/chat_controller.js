bb0490d1527265921348c03a1db6e5716ab10591
Adrian Perez <appperez@ucdavis.edu>
2024-12-05 19:57:43 -0800

    Add append message to chat controller

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -5,9 +5,7 @@ import  consumer  from "channels/consumer";
 export default class extends Controller {
   connect() {
     let chatId = this.element.dataset.chatId
-    console.log(chatId)
     this.sub = this.createActionCableChannel(chatId);
-    // console.log(this.sub)
   }
 
   disconnect() {
@@ -18,6 +16,7 @@ export default class extends Controller {
     return consumer.subscriptions.create({channel: "ChatChannel", chat_id: chatId }, {
       connected() {
         // Called when the subscription is ready for use on the server
+        // This was a test to see how the channel worked
         this.perform("get_user_data", { chat_id: chatId })
       },
 
@@ -27,7 +26,28 @@ export default class extends Controller {
 
       received(data) {
         // Called when there's incoming data on the websocket for this channel
-        console.log(data.user.name)
+        if (data.user) { // this should happen pretty much once when the user connects
+          this.current_user = data.user
+        }
+
+        if (data.message) {
+          this.append_message(data.message)
+        }
+      },
+
+      // Function to add a message when it is sent in action cable
+      append_message(message) {
+        console.log(message)
+        let chat_box = document.querySelector(`[data-chat-id="${message.chat_id}"]`)
+        let message_bubble = document.createElement("div")
+        message_bubble.classList.add("message")
+        message_bubble.textContent = message.message
+        if (message.sender_id == this.current_user.id) {
+          message_bubble.classList.add("right")
+        } else {
+          message_bubble.classList.add("left")
+        }
+        chat_box.appendChild(message_bubble)
       }
     });
   }
