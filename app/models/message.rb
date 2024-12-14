ac385f57af06bc76e7c68dcf62072f591971af77
Adrian Perez <appperez@ucdavis.edu>
2024-12-13 21:39:24 -0800

    Change how messages are added to the chat

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :chat
   belongs_to :sender, class_name: "User"
-
+  after_create_commit { broadcast_append_to self.chat }
   validates :message, presence: true
 end
