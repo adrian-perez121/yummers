5ef322a0fbdbf5f1fa8dc8a59abc118115132bb8
Adrian Perez <appperez@ucdavis.edu>
2024-12-13 22:52:31 -0800

    rework scroll to bottom controller

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -3,6 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="scroll"
 export default class extends Controller {
   connect() {
+
+    const messages = document.getElementById("messages")
+    // here we don't care about what is getting passed into the callback
+    const observer = new MutationObserver(() => {
+      this.scrollToBottom() // This is all we really want to happen
+    })
+    observer.observe(messages, { childList: true }) // look at messages' children
     this.scrollToBottom()
   }
 
